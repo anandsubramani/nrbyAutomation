c3a1ce00b5ef17a4af5c61e3ecf4ad2bbf478458
Anand Amirtharaj <anand@nrby.com>
2016-05-10 19:49:11 +0530

    Update nrby.sh

--- nrby_jenkins/nrby.sh
+++ nrby_jenkins/nrby.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 echo Please provide your Project directory : "$ppath"
 read ppath
-echo Please provide nrby_automation.xml path: "$xmlpath"
-read xmlpath
+xmlpath=$ppath/nrby_jenkins
 cd $xmlpath
 echo Please enter the Job name: "$jobname"
 read jobname
@@ -10,17 +9,20 @@ echo Please enter the Host name[eg:http://10.6.5.13:8080/]: "$hostname"
 read hostname
 java -jar jenkins-cli.jar -s $hostname create-job $jobname < nrby_automation.xml
 echo Job created successfully
-file="$ppath/target/surefire-reports/"
+file="$ppath"
 cd $file
+mkdir -p target/surefire-reports
+cd $file/target/surefire-reports/
 rm -f index.html
 cd $xmlpath
 java -jar jenkins-cli.jar -s $hostname build $jobname -s &
 while : ; do 
     [[ -f "$ppath/target/surefire-reports/index.html" ]] && break   
-    
 done
     cd $ppath/target/
-    jpath=$(locate .jenkins/jobs | grep /.jenkins/jobs$)
-    sleep 1
+    pwd
+    jepath=$(locate .jenkins/jobs | head -1)
+    echo $jepath
     zip -r reports.zip surefire-reports 
-    cp reports.zip $jpath/$jobname/workspace/
+    sleep 2
+    mv reports.zip $jepath/$jobname/workspace
